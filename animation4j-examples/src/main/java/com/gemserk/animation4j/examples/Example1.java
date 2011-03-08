82ed238b533418a0f8f8f55440603531b7a45240
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-08 13:16:07 -0200

    modified class Example1 to show the first example using the componentsengine-core Java2d classes.

--- animation4j-examples/src/main/java/com/gemserk/animation4j/examples/Example1.java
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/Example1.java
@@ -3,7 +3,17 @@ package com.gemserk.animation4j.examples;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import com.gemserk.animation4j.interpolator.FloatInterpolator;
+import com.gemserk.animation4j.interpolator.function.InterpolatorFunctionFactory;
+import com.gemserk.animation4j.timeline.TimelineAnimation;
+import com.gemserk.animation4j.timeline.TimelineAnimationBuilder;
+import com.gemserk.animation4j.timeline.TimelineValueBuilder;
 import com.gemserk.componentsengine.java2d.Java2dDesktopApplication;
 import com.gemserk.componentsengine.java2d.Java2dGame;
 import com.gemserk.componentsengine.java2d.Java2dModule;
@@ -11,8 +21,15 @@ import com.gemserk.componentsengine.java2d.input.KeyboardInput;
 import com.gemserk.componentsengine.java2d.input.MouseInput;
 import com.gemserk.componentsengine.java2d.render.CurrentGraphicsProvider;
 import com.gemserk.componentsengine.java2d.render.InitJava2dRenderer;
+import com.gemserk.componentsengine.java2d.render.Java2dImageRenderObject;
+import com.gemserk.componentsengine.java2d.render.Java2dRenderer;
 import com.gemserk.componentsengine.modules.BasicModule;
 import com.gemserk.componentsengine.modules.ResourcesManagerModule;
+import com.gemserk.resources.Resource;
+import com.gemserk.resources.ResourceManager;
+import com.gemserk.resources.dataloaders.DataLoader;
+import com.gemserk.resources.resourceloaders.CachedResourceLoader;
+import com.gemserk.resources.resourceloaders.ResourceLoaderImpl;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -42,25 +59,77 @@ public class Example1 extends Java2dDesktopApplication {
 		@Inject
 		MouseInput mouseInput;
 		
+
+		@SuppressWarnings("rawtypes")
 		@Inject
-		CurrentGraphicsProvider currentGraphicsProvider;
+		ResourceManager resourceManager;
+
+		Resource<Image> critterImageResource;
 
 		@Override
 		public void init() {
 			
+			resourceManager.add("Critter", new CachedResourceLoader<Image>(new ResourceLoaderImpl<Image>(new DataLoader<Image>() {
+
+				@Override
+				public Image load() {
+					try {
+						return ImageIO.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("critter.png"));
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			})));
+			
+			critterImageResource = resourceManager.get("Critter");
+			
+			animation = new TimelineAnimationBuilder() {
+				{
+					speed(2f);
+					started(false);
+
+					value("x", new TimelineValueBuilder<Float>().keyFrame(0, 150f, new FloatInterpolator(InterpolatorFunctionFactory.easeIn())).keyFrame(1000, 350f));
+					value("y", new TimelineValueBuilder<Float>().keyFrame(0, 150f));
+					value("angle", new TimelineValueBuilder<Float>().keyFrame(0, 0f, new FloatInterpolator(InterpolatorFunctionFactory.easeIn())).keyFrame(1000, (float) Math.PI / 2));
+				}
+			}.build();
+
+			animation.start(2, true);
+			
 		}
+		
+		@Inject
+		Java2dRenderer java2dRenderer;
+		
+		@Inject
+		CurrentGraphicsProvider currentGraphicsProvider;
+		
+		private TimelineAnimation animation;
 
 		@Override
 		public void render(Graphics2D graphics) {
 			graphics.setBackground(Color.blue);
 			graphics.clearRect(0, 0, 800, 600);
+			
+			Float x = animation.getValue("x");
+			Float y = animation.getValue("y");
+			Float angle = animation.getValue("angle");
+			
+			currentGraphicsProvider.setGraphics(graphics);
+			java2dRenderer.render(new Java2dImageRenderObject(1, critterImageResource.get(), x, y, 1, 1, angle));
+			
 		}
 
 		@Override
 		public void update(int delta) {
 			
+			animation.update(delta);
+			if (keyboardInput.keyDownOnce(KeyEvent.VK_ENTER)) {
+				animation.restart();
+			}
 		}
 		
 	}
 	
 }
+
