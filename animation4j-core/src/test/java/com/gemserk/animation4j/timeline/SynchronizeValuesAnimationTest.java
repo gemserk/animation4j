b48cf4f88143ab36f269aba6dab574bcc7f2bd9f
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-10 17:48:37 -0200

    extracted tests and classes for TimelineSynchronizerIterator

--- animation4j-core/src/test/java/com/gemserk/animation4j/timeline/SynchronizeValuesAnimationTest.java
+++ animation4j-core/src/test/java/com/gemserk/animation4j/timeline/SynchronizeValuesAnimationTest.java
@@ -1,19 +1,13 @@
 package com.gemserk.animation4j.timeline;
 
-import static org.junit.Assert.*;
-
-import java.util.HashMap;
-import java.util.Iterator;
-
-import org.hamcrest.core.AnyOf;
-import org.hamcrest.core.IsNull;
-import org.hamcrest.core.IsSame;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.gemserk.animation4j.timeline.TimelineSynchronizerIteratorTest.TimelineSynchronizerIterator;
 
 
 @RunWith(JMock.class)
@@ -27,12 +21,12 @@ public class SynchronizeValuesAnimationTest {
 	
 	public static class TimelineSynchronizer {
 		
-		private final SynchronizerIterator synchronizerIterator;
+		private final TimelineSynchronizerIterator timelineSynchronizerIterator;
 		
 		private final ObjectSynchronizer objectSynchronizer;
 
-		public TimelineSynchronizer(SynchronizerIterator synchronizerIterator, ObjectSynchronizer objectSynchronizer) {
-			this.synchronizerIterator = synchronizerIterator;
+		public TimelineSynchronizer(TimelineSynchronizerIterator timelineSynchronizerIterator, ObjectSynchronizer objectSynchronizer) {
+			this.timelineSynchronizerIterator = timelineSynchronizerIterator;
 			this.objectSynchronizer = objectSynchronizer;
 		}
 
@@ -41,8 +35,8 @@ public class SynchronizeValuesAnimationTest {
 		 * @param time
 		 */
 		public void syncrhonize(int time) {
-			while(synchronizerIterator.hasNext()) {
-				TimelineValue<Object> timelineValue = synchronizerIterator.next();
+			while(timelineSynchronizerIterator.hasNext()) {
+				TimelineValue<Object> timelineValue = timelineSynchronizerIterator.next();
 				String name = timelineValue.getName();
 				Object value = timelineValue.getValue(time);
 				objectSynchronizer.setValue(name, value);
@@ -55,16 +49,16 @@ public class SynchronizeValuesAnimationTest {
 	@SuppressWarnings("rawtypes")
 	public void shouldGetValuesFromIteratorAndSetToObject() {
 		
-		final SynchronizerIterator synchronizerIterator = mockery.mock(SynchronizerIterator.class);
+		final TimelineSynchronizerIterator timelineSynchronizerIterator = mockery.mock(TimelineSynchronizerIterator.class);
 		final TimelineValue timelineValue = mockery.mock(TimelineValue.class);
 		final ObjectSynchronizer objectSynchronizer = mockery.mock(ObjectSynchronizer.class);
 		
 		mockery.checking(new Expectations() {
 			{
-				oneOf(synchronizerIterator).hasNext();
+				oneOf(timelineSynchronizerIterator).hasNext();
 				will(returnValue(true));
 				
-				oneOf(synchronizerIterator).next();
+				oneOf(timelineSynchronizerIterator).next();
 				will(returnValue(timelineValue));
 				
 				oneOf(timelineValue).getName();
@@ -75,126 +69,14 @@ public class SynchronizeValuesAnimationTest {
 				
 				oneOf(objectSynchronizer).setValue("x", 100f);
 				
-				oneOf(synchronizerIterator).hasNext();
+				oneOf(timelineSynchronizerIterator).hasNext();
 				will(returnValue(false));
 			}
 		});
 		
-		TimelineSynchronizer timelineSynchronizer = new TimelineSynchronizer(synchronizerIterator, objectSynchronizer);
+		TimelineSynchronizer timelineSynchronizer = new TimelineSynchronizer(timelineSynchronizerIterator, objectSynchronizer);
 		
 		timelineSynchronizer.syncrhonize(10);
-		
-	}
-	
-	
-	public static interface SynchronizerIterator {
-		
-		<T> TimelineValue<T> next();
-		
-		boolean hasNext(); 
-		
-	}
-	
-	public static class SynchronizerIteratorImpl implements SynchronizerIterator {
-
-		private final Timeline timeline;
-		
-		private Iterator<String> iterator;
-
-		public SynchronizerIteratorImpl(Timeline timeline) {
-			this.timeline = timeline;
-			iterator = timeline.getTimelineValues().keySet().iterator();
-		}
-		
-		@SuppressWarnings("unchecked")
-		@Override
-		public <T> TimelineValue<T> next() {
-			return timeline.getTimelineValues().get(iterator.next());
-		}
-
-		@Override
-		public boolean hasNext() {
-			return iterator.hasNext();
-		}
-		
-	}
-	
-	@Test
-	public void hasNextShouldBeFalseWithEmptyValues() {
-		
-		final Timeline timeline = mockery.mock(Timeline.class);
-		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
-		
-		mockery.checking(new Expectations() {
-			{
-				ignoring(timeline).getTimelineValues();
-				will(returnValue(values));
-			}
-		});
-		
-		SynchronizerIterator synchronizerIterator = new SynchronizerIteratorImpl(timeline);
-		
-		assertFalse(synchronizerIterator.hasNext());
-		
-	}
-	
-	@Test
-	public void hasNextShouldBeTrueWhenNotEmptyValues() {
-		
-		final Timeline timeline = mockery.mock(Timeline.class);
-		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
-		
-		values.put("x", new TimelineValue());
-		
-		mockery.checking(new Expectations() {
-			{
-				ignoring(timeline).getTimelineValues();
-				will(returnValue(values));
-			}
-		});
-		
-		SynchronizerIterator synchronizerIterator = new SynchronizerIteratorImpl(timeline);
-		
-		assertTrue(synchronizerIterator.hasNext());
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void nextShouldReturnFirstElement() {
-		
-		final Timeline timeline = mockery.mock(Timeline.class);
-		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
-		
-		TimelineValue firstTimelineValue = new TimelineValue();
-		TimelineValue secondTimelineValue = new TimelineValue();
-		
-		values.put("a", firstTimelineValue);
-		values.put("b", secondTimelineValue);
-		
-		mockery.checking(new Expectations() {
-			{
-				ignoring(timeline).getTimelineValues();
-				will(returnValue(values));
-			}
-		});
-		
-		SynchronizerIterator synchronizerIterator = new SynchronizerIteratorImpl(timeline);
-		
-		assertTrue(synchronizerIterator.hasNext());
-		TimelineValue<Object> currentElement = synchronizerIterator.next();
-		
-		assertThat(currentElement, IsNull.notNullValue());
-		assertThat(currentElement, AnyOf.anyOf(IsSame.sameInstance(firstTimelineValue), IsSame.sameInstance(secondTimelineValue)));
-		
-		assertTrue(synchronizerIterator.hasNext());
-
-		currentElement = synchronizerIterator.next();
-		
-		assertThat(currentElement, IsNull.notNullValue());
-		assertThat(currentElement, AnyOf.anyOf(IsSame.sameInstance(firstTimelineValue), IsSame.sameInstance(secondTimelineValue)));
-		
-		assertFalse(synchronizerIterator.hasNext());
 		
 	}
 	
---
+++ animation4j-core/src/test/java/com/gemserk/animation4j/timeline/TimelineSynchronizerIteratorTest.java
@@ -0,0 +1,139 @@
+package com.gemserk.animation4j.timeline;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.hamcrest.core.AnyOf;
+import org.hamcrest.core.IsNull;
+import org.hamcrest.core.IsSame;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(JMock.class)
+public class TimelineSynchronizerIteratorTest {
+
+	Mockery mockery = new Mockery() {
+		{
+			setImposteriser(ClassImposteriser.INSTANCE);
+		}
+	};
+	
+	public static interface TimelineSynchronizerIterator {
+		
+		<T> TimelineValue<T> next();
+		
+		boolean hasNext(); 
+		
+	}
+	
+	public static class TimelineSynchronizerIteratorImpl implements TimelineSynchronizerIterator {
+
+		private final Timeline timeline;
+		
+		private Iterator<String> iterator;
+
+		public TimelineSynchronizerIteratorImpl(Timeline timeline) {
+			this.timeline = timeline;
+			iterator = timeline.getTimelineValues().keySet().iterator();
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public <T> TimelineValue<T> next() {
+			return timeline.getTimelineValues().get(iterator.next());
+		}
+
+		@Override
+		public boolean hasNext() {
+			return iterator.hasNext();
+		}
+		
+	}
+	
+	@Test
+	public void hasNextShouldBeFalseWithEmptyValues() {
+		
+		final Timeline timeline = mockery.mock(Timeline.class);
+		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
+		
+		mockery.checking(new Expectations() {
+			{
+				ignoring(timeline).getTimelineValues();
+				will(returnValue(values));
+			}
+		});
+		
+		TimelineSynchronizerIterator timelineSynchronizerIterator = new TimelineSynchronizerIteratorImpl(timeline);
+		
+		assertFalse(timelineSynchronizerIterator.hasNext());
+		
+	}
+	
+	@Test
+	public void hasNextShouldBeTrueWhenNotEmptyValues() {
+		
+		final Timeline timeline = mockery.mock(Timeline.class);
+		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
+		
+		values.put("x", new TimelineValue());
+		
+		mockery.checking(new Expectations() {
+			{
+				ignoring(timeline).getTimelineValues();
+				will(returnValue(values));
+			}
+		});
+		
+		TimelineSynchronizerIterator timelineSynchronizerIterator = new TimelineSynchronizerIteratorImpl(timeline);
+		
+		assertTrue(timelineSynchronizerIterator.hasNext());
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void nextShouldReturnFirstElement() {
+		
+		final Timeline timeline = mockery.mock(Timeline.class);
+		final HashMap<String, TimelineValue> values = new HashMap<String, TimelineValue>();
+		
+		TimelineValue firstTimelineValue = new TimelineValue();
+		TimelineValue secondTimelineValue = new TimelineValue();
+		
+		values.put("a", firstTimelineValue);
+		values.put("b", secondTimelineValue);
+		
+		mockery.checking(new Expectations() {
+			{
+				ignoring(timeline).getTimelineValues();
+				will(returnValue(values));
+			}
+		});
+		
+		TimelineSynchronizerIterator timelineSynchronizerIterator = new TimelineSynchronizerIteratorImpl(timeline);
+		
+		assertTrue(timelineSynchronizerIterator.hasNext());
+		TimelineValue<Object> currentElement = timelineSynchronizerIterator.next();
+		
+		assertThat(currentElement, IsNull.notNullValue());
+		assertThat(currentElement, AnyOf.anyOf(IsSame.sameInstance(firstTimelineValue), IsSame.sameInstance(secondTimelineValue)));
+		
+		assertTrue(timelineSynchronizerIterator.hasNext());
+
+		currentElement = timelineSynchronizerIterator.next();
+		
+		assertThat(currentElement, IsNull.notNullValue());
+		assertThat(currentElement, AnyOf.anyOf(IsSame.sameInstance(firstTimelineValue), IsSame.sameInstance(secondTimelineValue)));
+		
+		assertFalse(timelineSynchronizerIterator.hasNext());
+		
+	}
+	
+}
