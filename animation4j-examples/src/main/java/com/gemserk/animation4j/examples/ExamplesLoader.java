555ca30bc551cad872e7c638f485e7777afffc60
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-10 13:47:36 -0200

    modified ExamplesLoader to start a new thread for "app.start()" to avoid blocking the AWT events.

--- animation4j-examples/src/main/java/com/gemserk/animation4j/examples/ExamplesLoader.java
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/ExamplesLoader.java
@@ -5,67 +5,69 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.lang.reflect.Constructor;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import com.gemserk.componentsengine.java2d.Java2dDesktopApplication;
 
-public class ExamplesLoader  {
-	
+@SuppressWarnings("serial")
+public class ExamplesLoader {
+
 	static class Option {
-		
-		Class classToLoad;
-		
+
+		Java2dDesktopApplication app;
+
 		String name;
-		
-		public Option(String name, Class classToLoad) {
+
+		public Option(String name, Java2dDesktopApplication app) {
 			this.name = name;
-			this.classToLoad = classToLoad;
+			this.app = app;
 		}
-		
+
 		@Override
 		public String toString() {
 			return name;
 		}
-		
+
 	}
-	
-	static Option[] options = new Option[]{ //
-		new Option("Example1", Example1.class), //
-		new Option("Example2", Example2.class), //
-	};
-	
-	static Option selectedOption = options[0]; 
-	
+
+	static Option[] options;
+
+	static Option selectedOption;
+
 	public static void main(String[] arguments) {
-		
-		new JFrame() {{
-			
-			addWindowListener(new WindowAdapter() {
-				
-				public void windowClosing(WindowEvent e) {
-					System.exit(0);
-				}
-				
-			});
-			
-			setSize(340, 140);
-			setLocation(300, 300);
-			
-			setLayout(new GridLayout(3, 1));
-			
-			add(new JLabel("Select prototype"));
-			
-			add(new JComboBox(options){{
-				
+
+		final JDialog mainFrame = new JDialog() {
+			{
+
+				setSize(340, 140);
+				setLocation(300, 300);
+
+				setLayout(new GridLayout(3, 1));
+
+				add(new JLabel("Select example"));
+
+			}
+		};
+
+		mainFrame.setVisible(true);
+
+		options = new Option[] { //
+				new Option("Example1", new Example1()), //
+				new Option("Example2", new Example2()), //
+		};
+
+		selectedOption = options[0];
+
+		mainFrame.add(new JComboBox(options) {
+			{
+
 				addItemListener(new ItemListener() {
-					
+
 					@Override
 					public void itemStateChanged(ItemEvent event) {
 						if (event.getStateChange() == ItemEvent.SELECTED) {
@@ -74,39 +76,44 @@ public class ExamplesLoader  {
 						}
 					}
 				});
-				
-			}});
-			
-			add(new JButton() {{ 
-				
+
+			}
+		});
+
+		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		mainFrame.add(new JButton() {
+			{
 				setText("Play");
 				addActionListener(new ActionListener() {
-					
+
 					@Override
 					public void actionPerformed(ActionEvent event) {
-						
+
 						System.out.println("Starting " + selectedOption.name);
-						
+
 						try {
-							
-							Class<? extends Java2dDesktopApplication> prototypeClass = selectedOption.classToLoad;
-							Constructor<? extends Java2dDesktopApplication> constructor = prototypeClass.getConstructor();
-							
-							Java2dDesktopApplication exampleInstance = constructor.newInstance();
+							mainFrame.setVisible(false);
+
+							final Java2dDesktopApplication exampleInstance = selectedOption.app;
 							
 							exampleInstance.init();
-							exampleInstance.start();
 							
+							new Thread() { 
+								public void run() {
+									exampleInstance.start();
+								};
+							}.start();
+
 						} catch (Exception ex) {
 							throw new RuntimeException("failed to start prototype: " + selectedOption.name, ex);
 						}
-						
+
 					}
 				});
-				
-			}});
-			
-		}}.setVisible(true);
+
+			}
+		});
 		
 	}
 
