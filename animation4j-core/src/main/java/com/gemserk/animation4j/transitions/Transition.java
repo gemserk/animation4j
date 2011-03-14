f34a8c546a02fd065c2568391d6ca4cc8b145cb0
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-14 13:15:37 -0300

    added missing file of the Transition interface

---
+++ animation4j-core/src/main/java/com/gemserk/animation4j/transitions/Transition.java
@@ -0,0 +1,36 @@
+package com.gemserk.animation4j.transitions;
+
+/**
+ * Provides an interface of a transition of a value.
+ * 
+ * @param <T> - The type of the transition value.
+ * @author acoppes
+ */
+public interface Transition<T> {
+
+	/**
+	 * Returns the current value of the transition.
+	 * 
+	 * @return - The current value of the transition.
+	 */
+	T get();
+
+	/**
+	 * Start an interpolation from a to b in the specified default time.
+	 * 
+	 * @param t
+	 *            - The wanted new value.
+	 */
+	void set(T t);
+
+	/**
+	 * Start an interpolation from a to b in the specified time.
+	 * 
+	 * @param t
+	 *            - The wanted new value.
+	 * @param time
+	 *            - The time to set the new value.
+	 */
+	void set(T t, int time);
+
+}
