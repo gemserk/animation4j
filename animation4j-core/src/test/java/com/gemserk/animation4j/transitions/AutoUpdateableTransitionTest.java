fd38ba86564e0102c3d323999f2f8631c61eff57
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-14 13:18:30 -0300

    added the test for AutoUpdateableTransition based on the InterpolatedPropertyTest

---
+++ animation4j-core/src/test/java/com/gemserk/animation4j/transitions/AutoUpdateableTransitionTest.java
@@ -0,0 +1,56 @@
+package com.gemserk.animation4j.transitions;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gemserk.animation4j.interpolator.FloatInterpolator;
+import com.gemserk.animation4j.interpolator.Interpolator;
+import com.gemserk.animation4j.interpolator.function.InterpolatorFunctionFactory;
+import com.gemserk.animation4j.time.UpdateableTimeProvider;
+
+
+public class AutoUpdateableTransitionTest {
+
+	private Interpolator<Float> interpolator;
+	
+	private UpdateableTimeProvider timeProvider;
+	
+	@Before
+	public void setUp() {
+		timeProvider = new UpdateableTimeProvider();
+		interpolator = new FloatInterpolator(InterpolatorFunctionFactory.linear());
+	}
+	
+	@Test
+	public void shouldReturnFirstValueWhenNoTimePassed() {
+		AutoUpdateableTransition<Float> transition = new AutoUpdateableTransition<Float>(new UpdateableTransition<Float>(100f, interpolator), 1f, timeProvider);
+		assertThat(transition.get(), IsEqual.equalTo(100f));
+		transition.set(200f);
+		assertThat(transition.get(), IsEqual.equalTo(100f));
+	}
+	
+	@Test
+	public void shouldReturnSecondValueWhenTimePassed() {
+		AutoUpdateableTransition<Float> transition = new AutoUpdateableTransition<Float>(new UpdateableTransition<Float>(100f, interpolator), 1f, timeProvider);
+		assertThat(transition.get(), IsEqual.equalTo(100f));
+		transition.set(200f);
+		timeProvider.update(10000);
+		assertThat(transition.get(), IsEqual.equalTo(200f));
+	}
+	
+	@Test
+	public void shouldReturnInterpolatedValue() {
+		AutoUpdateableTransition<Float> transition = new AutoUpdateableTransition<Float>(new UpdateableTransition<Float>(100f, interpolator), 0.01f, timeProvider);
+		assertThat(transition.get(), IsEqual.equalTo(100f));
+		transition.set(200f);
+		timeProvider.update(50);
+		assertThat(transition.get(), IsEqual.equalTo(150f));
+		timeProvider.update(25);
+		assertThat(transition.get(), IsEqual.equalTo(175f));
+	}
+
+	
+}
