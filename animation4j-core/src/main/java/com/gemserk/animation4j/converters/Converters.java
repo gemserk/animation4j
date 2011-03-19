7ada5c2de3f7bf26da1de87213dc73acdebc1bec
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-19 10:45:24 -0300

    modified the converters from Converters class to be static and stateles to be reused between different GenericInterpolators

--- animation4j-core/src/main/java/com/gemserk/animation4j/converters/Converters.java
+++ animation4j-core/src/main/java/com/gemserk/animation4j/converters/Converters.java
@@ -4,80 +4,81 @@ import java.awt.Color;
 import java.awt.geom.Point2D;
 
 /**
- * Provides type converters for common classes like Color and Point2D. 
+ * Provides type converters for common classes like Color and Point2D.
  * @author acoppes
  */
 public class Converters {
 	
-	public static TypeConverter<Float> floatConverter() {
-		
-		// not recommended to use, it will be boxing and unboxing from float to Float and vice versa. 
-		
-		return new TypeConverter<Float>() {
-			
-			@Override
-			public float[] copyFromObject(Float f, float[] x) {
-				if (x == null)
-					x = new float[1];
-				x[0] = f.floatValue();
-				return x;
-			}
+	private static TypeConverter<Float> floatConverter = new TypeConverter<Float>() {
 
-			@Override
-			public Float copyToObject(Float f, float[] x) {
-				return Float.valueOf(x[0]);
-			}
-		};
+		@Override
+		public float[] copyFromObject(Float f, float[] x) {
+			if (x == null)
+				x = new float[1];
+			x[0] = f.floatValue();
+			return x;
+		}
+
+		@Override
+		public Float copyToObject(Float f, float[] x) {
+			// as Float is immutable, it will return a new float each time.
+			return Float.valueOf(x[0]);
+		}
 		
+	};
+	private static TypeConverter<Point2D> point2dConverter = new TypeConverter<Point2D>() {
+
+		@Override
+		public float[] copyFromObject(Point2D p, float[] x) {
+			if (x == null)
+				x = new float[2];
+			x[0] = (float) p.getX();
+			x[1] = (float) p.getY();
+			return x;
+		}
+
+		@Override
+		public Point2D copyToObject(Point2D p, float[] x) {
+			if (p == null)
+				p = new Point2D.Float(0f, 0f);
+			p.setLocation(x[0], x[1]);
+			return p;
+		}
+
+	};
+	private static TypeConverter<Color> colorConverter = new TypeConverter<Color>() {
+
+		@Override
+		public float[] copyFromObject(Color object, float[] x) {
+			if (x == null)
+				x = new float[4];
+			object.getRGBComponents(x);
+			return x;
+		}
+
+		@Override
+		public Color copyToObject(Color object, float[] x) {
+			// as color is immutable, it will return a new color each time.
+			return new Color(x[0], x[1], x[2], x[3]);
+		}
+
+	};
+
+	/**
+	 * This converter will be boxing and unboxing from float to Float and vice versa each time a method is called, so it is not recommended to use, 
+	 * we recommend you to use a TypeConverter of a FloatValue mutable implementation instead.
+	 */
+	public static TypeConverter<Float> floatConverter() {
+		// not recommended to use, it will be boxing and unboxing from float to Float and vice versa.
+		return floatConverter;
 	}
 
 	public static TypeConverter<Point2D> point2d() {
-
-		// the type converter could be reused if it returns a new point each call or if it uses a pool of objects, else
-		// use a new type converter for each new transition
-
-		return new TypeConverter<Point2D>() {
-			
-			@Override
-			public float[] copyFromObject(Point2D p, float[] x) {
-				if (x == null)
-					x = new float[2];
-				x[0] = (float) p.getX();
-				x[1] = (float) p.getY();
-				return x;
-			}
-
-			@Override
-			public Point2D copyToObject(Point2D p, float[] x) {
-				if (p == null)
-					p = new Point2D.Float(0f, 0f);;
-				p.setLocation(x[0], x[1]);
-				return p;
-			}
-
-		};
+		return point2dConverter;
 	}
 
 	public static TypeConverter<Color> color() {
-		
-		// in this case, as color is not mutable, we have to return a new color each time. 
-		
-		return new TypeConverter<Color>() {
-			
-			@Override
-			public float[] copyFromObject(Color object, float[] x) {
-				if (x == null)
-					x = new float[4];
-				object.getRGBComponents(x);
-				return x;
-			}
-
-			@Override
-			public Color copyToObject(Color object, float[] x) {
-				return new Color(x[0], x[1], x[2], x[3]);
-			}
-			
-		};
+		return colorConverter;
 	}
 
 }
