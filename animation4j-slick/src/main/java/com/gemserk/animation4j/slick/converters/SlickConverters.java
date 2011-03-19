acf342341598a301d8910bfd397af56f9f08ce60
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-19 10:46:37 -0300

    modified slick converters also, for the same reason of the previous commit

--- animation4j-slick/src/main/java/com/gemserk/animation4j/slick/converters/SlickConverters.java
+++ animation4j-slick/src/main/java/com/gemserk/animation4j/slick/converters/SlickConverters.java
@@ -7,59 +7,59 @@ import com.gemserk.animation4j.converters.TypeConverter;
 
 public class SlickConverters {
 	
+	private static TypeConverter<Vector2f> vector2fConverter = new TypeConverter<Vector2f>() {
+		
+		@Override
+		public float[] copyFromObject(Vector2f v, float[] x) {
+			if (x == null)
+				x = new float[2];
+			x[0] = v.x;
+			x[1] = v.y;
+			return x;
+		}
+		
+		@Override
+		public Vector2f copyToObject(Vector2f v, float[] x) {
+			if (v == null)
+				v = new Vector2f();
+			v.x = x[0];
+			v.y = x[1];
+			return v;
+		}
+	};
+	
+	private static TypeConverter<Color> colorConverter = new TypeConverter<Color>() {
+		
+		@Override
+		public float[] copyFromObject(Color color, float[] x) {
+			if (x == null)
+				x = new float[4];
+			x[0] = color.r;
+			x[1] = color.g;
+			x[2] = color.b;
+			x[3] = color.a;
+			return x;
+		}
+
+		@Override
+		public Color copyToObject(Color color, float[] x) {
+			if (color == null)
+				color = new Color(0);
+			color.r = x[0];
+			color.g = x[1];
+			color.b = x[2];
+			color.a = x[3];
+			return color;
+		}
+		
+	};
+
 	public static TypeConverter<Vector2f> vector2f() {
-		
-		return new TypeConverter<Vector2f>() {
-			
-			@Override
-			public float[] copyFromObject(Vector2f v, float[] x) {
-				if (x == null)
-					x = new float[2];
-				x[0] = v.x;
-				x[1] = v.y;
-				return x;
-			}
-			
-			@Override
-			public Vector2f copyToObject(Vector2f v, float[] x) {
-				if (v == null)
-					v = new Vector2f();
-				v.x = x[0];
-				v.y = x[1];
-				return v;
-			}
-		};
-		
+		return vector2fConverter;
 	}
 	
 	public static TypeConverter<Color> color() {
-
-		return new TypeConverter<Color>() {
-			
-			@Override
-			public float[] copyFromObject(Color color, float[] x) {
-				if (x == null)
-					x = new float[4];
-				x[0] = color.r;
-				x[1] = color.g;
-				x[2] = color.b;
-				x[3] = color.a;
-				return x;
-			}
-
-			@Override
-			public Color copyToObject(Color color, float[] x) {
-				if (color == null)
-					color = new Color(0);
-				color.r = x[0];
-				color.g = x[1];
-				color.b = x[2];
-				color.a = x[3];
-				return color;
-			}
-			
-		};
-		
+		return colorConverter;
 	}
 
 }
