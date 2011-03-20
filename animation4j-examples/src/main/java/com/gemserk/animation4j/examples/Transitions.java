88cf06804af358e4b5ac7da24c8a7cf6327e3468
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-20 11:48:21 -0300

    added new headless example

--- animation4j-examples/src/main/java/com/gemserk/animation4j/examples/Transitions.java
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/Transitions.java
@@ -1,18 +1,36 @@
 package com.gemserk.animation4j.examples;
 
 import com.gemserk.animation4j.interpolator.Interpolator;
+import com.gemserk.animation4j.time.SystemTimeProvider;
+import com.gemserk.animation4j.time.TimeProvider;
 import com.gemserk.animation4j.transitions.AutoUpdateableTransition;
 import com.gemserk.animation4j.transitions.Transition;
 import com.gemserk.animation4j.transitions.UpdateableTransition;
 
 public class Transitions {
 
+	private static float ms = 1f / 1000f;
+	
+	public static TimeProvider timeProvider = new SystemTimeProvider();
+
 	public static <T> Transition<T> updateable(T startValue, Interpolator<T> interpolator) {
 		return new UpdateableTransition<T>(startValue, interpolator);
 	}
+	
+	public static <T> Transition<T> transition(T startValue, Interpolator<T> interpolator) {
+		return transition(startValue, interpolator, ms);
+	}
+
+	public static <T> Transition<T> transition(T startValue, T endValue, Interpolator<T> interpolator) {
+		return transition(startValue, endValue, interpolator, ms);
+	}
 
 	public static <T> Transition<T> transition(T startValue, Interpolator<T> interpolator, float speed) {
-		return new AutoUpdateableTransition<T>(startValue, interpolator, speed);
+		return new AutoUpdateableTransition<T>(startValue, interpolator, speed, timeProvider);
+	}
+
+	public static <T> Transition<T> transition(T startValue, T endValue, Interpolator<T> interpolator, float speed) {
+		return new AutoUpdateableTransition<T>(startValue, endValue, interpolator, speed, timeProvider);
 	}
 
 }
---
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/TransitionsExample.java
@@ -0,0 +1,79 @@
+package com.gemserk.animation4j.examples;
+
+import com.gemserk.animation4j.converters.TypeConverter;
+import com.gemserk.animation4j.interpolator.FloatArrayInterpolator;
+import com.gemserk.animation4j.interpolator.GenericInterpolator;
+import com.gemserk.animation4j.interpolator.Interpolator;
+import com.gemserk.animation4j.interpolator.function.InterpolatorFunctionFactory;
+import com.gemserk.animation4j.transitions.Transition;
+
+public class TransitionsExample {
+	
+	static class Vector2f {
+		
+		float x,y;
+		
+		public Vector2f(float x, float y) {
+			set(x,y);
+		}
+		
+		public void set(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public String toString() {
+			return "vector(" + x + ", " + y + ")";
+		}
+		
+	}
+	
+	static class Vector2fConverter implements TypeConverter<Vector2f> {
+
+		@Override
+		public float[] copyFromObject(Vector2f v, float[] x) {
+			if (x == null)
+				x = new float[2];
+			x[0] = v.x;
+			x[1] = v.y;
+			return x;
+			
+		}
+
+		@Override
+		public Vector2f copyToObject(Vector2f v, float[] x) {
+			if (v == null)
+				v = new Vector2f(0, 0);
+			v.x = x[0];
+			v.y = x[1];
+			return v;
+		}
+		
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		// this one could be reused any time we want, should be stateless.
+		Vector2fConverter vector2fConverter = new Vector2fConverter();
+		
+		FloatArrayInterpolator twoVariableInterpolator = new FloatArrayInterpolator(2, InterpolatorFunctionFactory.linear(), InterpolatorFunctionFactory.linear());
+		
+		Interpolator<Vector2f> vector2fInterpolator = new GenericInterpolator<Vector2f>(vector2fConverter, twoVariableInterpolator);
+		
+		// By default, transitions use system time provider.
+		Transition<Vector2f> transition = Transitions.transition(new Vector2f(0f, 0f), vector2fInterpolator);
+		
+		System.out.println("Transition value: " + transition.get());
+		
+		transition.set(new Vector2f(100f, 100f), 500);
+		
+		System.out.println("Transition value: " + transition.get());
+		Thread.sleep(300);
+		System.out.println("Transition value: " + transition.get());
+		Thread.sleep(200);
+		System.out.println("Transition value: " + transition.get());
+		
+	}
+
+}
