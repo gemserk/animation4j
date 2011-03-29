f543f30abf4772d9f5fceb48261f22d05829c40b
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-28 21:09:46 -0300

    added a bit of javadoc to Interpolators factory class, but it should be removed because it shouldnt be used

--- animation4j-core/src/main/java/com/gemserk/animation4j/interpolator/Interpolators.java
+++ animation4j-core/src/main/java/com/gemserk/animation4j/interpolator/Interpolators.java
@@ -8,7 +8,6 @@ import com.gemserk.animation4j.interpolator.function.InterpolationFunction;
  * Provides factory methods to simplify Interpolator<T> creation.
  * 
  * @author acoppes
- * 
  */
 public class Interpolators {
 
@@ -26,10 +25,24 @@ public class Interpolators {
 		return interpolator(Converters.floatValue(), function);
 	}
 
+	/**
+	 * Returns an Interpolator<T> with internal cache, using the specified type converter.
+	 * 
+	 * @param typeConverter
+	 *            The TypeConverter to convert your type to float[] and vice versa.
+	 */
 	public static <T> Interpolator<T> interpolator(TypeConverter<T> typeConverter) {
 		return new GenericInterpolator<T>(typeConverter);
 	}
 
+	/**
+	 * Returns an Interpolator<T> with internal cache, using the specified type converter.
+	 * 
+	 * @param typeConverter
+	 *            The TypeConverter to convert your type to float[] and vice versa.
+	 * @param functions
+	 *            The functions to be used when interpolating the values.
+	 */
 	public static <T> Interpolator<T> interpolator(TypeConverter<T> typeConverter, InterpolationFunction... functions) {
 		return new GenericInterpolator<T>(typeConverter, functions);
 	}
