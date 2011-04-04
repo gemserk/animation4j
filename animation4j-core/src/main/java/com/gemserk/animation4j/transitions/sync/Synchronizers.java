44dcc81fe749599c72faf5d763ecad054f8a9620
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-04 16:20:36 -0300

    added a bit of javadocs for Synchronizers class and modified parameter time to duration

--- animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/Synchronizers.java
+++ animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/Synchronizers.java
@@ -21,17 +21,24 @@ public class Synchronizers {
 
 	}
 
-	public static void transition(Object object, String field, Object endValue, int time) {
+	/**
+	 * Starts a transition and a synchronizer to modify the specified object's field through the transition.
+	 * @param object The container of the field to be modified.
+	 * @param field The name of the field which contains the object to be modified.
+	 * @param endValue The end value of the transition.
+	 * @param duration The duration of the transition.
+	 */
+	public static void transition(Object object, String field, Object endValue, int duration) {
 
 		try {
 			String getterName = ReflectionUtils.getGetterName(field);
 			Method getterMethod = ReflectionUtils.findMethod(object.getClass(), getterName);
-			
+
 			if (getterMethod == null)
 				throw new RuntimeException();
-			
+
 			Object startValue = getterMethod.invoke(object, (Object[]) null);
-			transition(object, field, startValue, endValue, time);
+			transition(object, field, startValue, endValue, duration);
 		} catch (Exception e) {
 			throw new RuntimeException("get" + ReflectionUtils.capitalize(field) + "() method not found in " + object.getClass(), e);
 		}
@@ -42,16 +49,29 @@ public class Synchronizers {
 		synchronizedTransitionManager.synchronize();
 	}
 
-	public static void transition(final Object object, Object endValue, int time) {
+	/**
+	 * Starts a transition and a synchronizer of the transition current value with the specified object. The object must be <b>mutable</b> in order to be modified.
+	 * 
+	 * @param object The <b>mutable</b> object to be modified in through the transition. 
+	 * @param endValue The end value of the transition.
+	 * @param duration The duration of the transition.
+	 */
+	public static void transition(final Object object, Object endValue, int duration) {
 		final Transition<Object> transition = Transitions.transition(object);
-		transition.set(endValue, time);
+		transition.set(endValue, duration);
 		transition(object, transition);
 	}
-	
+
+	/**
+	 * Starts a transition and a synchronizer of the transition current value with the specified object. The object must be <b>mutable</b> in order to be modified.
+	 * 
+	 * @param object The <b>mutable</b> object to be modified in through the transition. 
+	 * @param transition The transition to use to modify the object.
+	 */
 	public static void transition(final Object object, final Transition transition) {
-		
+
 		synchronizedTransitionManager.handle(new TransitionObjectSynchronizer() {
-			
+
 			@Override
 			public void synchronize() {
 				TypeConverter typeConverter = Converters.converter(object.getClass());
@@ -59,14 +79,14 @@ public class Synchronizers {
 				float[] x = typeConverter.copyFromObject(currentValue, null);
 				typeConverter.copyToObject(object, x);
 			}
-			
+
 			@Override
 			public boolean isFinished() {
 				return !transition.isTransitioning();
 			}
-			
+
 		});
-		
+
 	}
 
 }
