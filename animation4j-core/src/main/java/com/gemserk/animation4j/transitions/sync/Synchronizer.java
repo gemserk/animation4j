00f4971e925b7ae53f3de3a120372a1442d27c6a
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-12 13:59:47 -0300

    added new Synchronizer class with all the stuff to define synchronized transitions and modified Synchronizers static class to use an internal instance of Synchronizer class

---
+++ animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/Synchronizer.java
@@ -0,0 +1,78 @@
+package com.gemserk.animation4j.transitions.sync;
+
+import com.gemserk.animation4j.converters.Converters;
+import com.gemserk.animation4j.converters.TypeConverter;
+import com.gemserk.animation4j.transitions.Transition;
+import com.gemserk.animation4j.transitions.Transitions.TransitionBuilder;
+import com.gemserk.animation4j.transitions.event.TransitionEventHandler;
+import com.gemserk.animation4j.transitions.event.TransitionMonitorBuilder;
+
+public class Synchronizer {
+
+	private SynchronizedTransitionManager synchronizedTransitionManager = new SynchronizedTransitionManager();
+
+	private TransitionHandlersManager transitionHandlersManager = new TransitionHandlersManager();
+
+	private TransitionMonitorBuilder transitionMonitorBuilder = new TransitionMonitorBuilder();
+
+	/**
+	 * Performs the synchronization of all the objects with the corresponding transitions registered by calling transition() method.
+	 */
+	public void synchronize() {
+		synchronizedTransitionManager.synchronize();
+		transitionHandlersManager.update();
+	}
+
+	/**
+	 * Starts a transition and a synchronizer to modify the specified object's field through the transition.
+	 * 
+	 * @param object
+	 *            The container of the field to be modified.
+	 * @param field
+	 *            The name of the field which contains the object to be modified.
+	 * @param transition
+	 *            The transition to synchronize with the object field.
+	 */
+	public void transition(Object object, String field, Transition transition) {
+		synchronizedTransitionManager.handle(new TransitionReflectionObjectSynchronizer(transition, object, field));
+	}
+
+	public void transition(Object object, TransitionBuilder transitionBuilder) {
+		transition(object, transitionBuilder.build());
+	}
+
+	/**
+	 * Starts a transition and a synchronizer of the transition current value with the specified object. The object must be <b>mutable</b> in order to be modified.
+	 * 
+	 * @param object
+	 *            The <b>mutable</b> object to be modified in through the transition.
+	 * @param transition
+	 *            The transition to use to modify the object.
+	 */
+	public void transition(final Object object, final Transition transition) {
+
+		synchronizedTransitionManager.handle(new TransitionObjectSynchronizer() {
+
+			@Override
+			public void synchronize() {
+				TypeConverter typeConverter = Converters.converter(object.getClass());
+				Object currentValue = transition.get();
+				float[] x = typeConverter.copyFromObject(currentValue, null);
+				typeConverter.copyToObject(object, x);
+			}
+
+			@Override
+			public boolean isFinished() {
+				return !transition.isTransitioning();
+			}
+
+		});
+
+	}
+
+	public void transition(Object object, Transition transition, TransitionEventHandler transitionEventHandler) {
+		transition(object, transition);
+		transitionHandlersManager.handle(transitionMonitorBuilder.with(transitionEventHandler).monitor(transition).build());
+	}
+
+}
--- animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/Synchronizers.java
+++ animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/Synchronizers.java
@@ -2,47 +2,47 @@ package com.gemserk.animation4j.transitions.sync;
 
 import java.lang.reflect.Method;
 
-import com.gemserk.animation4j.converters.Converters;
-import com.gemserk.animation4j.converters.TypeConverter;
 import com.gemserk.animation4j.reflection.ReflectionUtils;
 import com.gemserk.animation4j.transitions.Transition;
 import com.gemserk.animation4j.transitions.Transitions;
 import com.gemserk.animation4j.transitions.event.TransitionEventHandler;
-import com.gemserk.animation4j.transitions.event.TransitionMonitorBuilder;
 
 public class Synchronizers {
 
-	private static SynchronizedTransitionManager synchronizedTransitionManager = new SynchronizedTransitionManager();
-	
-	private static TransitionHandlersManager transitionHandlersManager = new TransitionHandlersManager();
-
-	private static TransitionMonitorBuilder transitionMonitorBuilder = new TransitionMonitorBuilder();
+	private static Synchronizer synchronizer = new Synchronizer();
 
 	/**
 	 * Starts a transition and a synchronizer to modify the specified object's field through the transition.
-	 * @param object The container of the field to be modified.
-	 * @param field The name of the field which contains the object to be modified.
-	 * @param startValue The start value of the transition.
-	 * @param endValue The end value of the transition.
-	 * @param duration The duration of the transition.
+	 * 
+	 * @param object
+	 *            The container of the field to be modified.
+	 * @param field
+	 *            The name of the field which contains the object to be modified.
+	 * @param startValue
+	 *            The start value of the transition.
+	 * @param endValue
+	 *            The end value of the transition.
+	 * @param duration
+	 *            The duration of the transition.
 	 */
 	public static void transition(Object object, String field, Object startValue, Object endValue, int duration) {
-		Transition<Object> transition = Transitions.transition(startValue);
-		transition.set(endValue, duration);
-		transition(object, field, transition);
+		synchronizer.transition(object, field, Transitions.transitionBuilder(startValue).end(endValue).time(duration).build());
 	}
 
 	/**
 	 * Starts a transition and a synchronizer to modify the specified object's field through the transition.
-	 * @param object The container of the field to be modified.
-	 * @param field The name of the field which contains the object to be modified.
-	 * @param endValue The end value of the transition.
-	 * @param duration The duration of the transition.
+	 * 
+	 * @param object
+	 *            The container of the field to be modified.
+	 * @param field
+	 *            The name of the field which contains the object to be modified.
+	 * @param endValue
+	 *            The end value of the transition.
+	 * @param duration
+	 *            The duration of the transition.
 	 */
 	public static void transition(Object object, String field, Object endValue, int duration) {
-
 		// this method seem strange here, why making all the reflection stuff ?
-		
 		try {
 			String getterName = ReflectionUtils.getGetterName(field);
 			Method getterMethod = ReflectionUtils.findMethod(object.getClass(), getterName);
@@ -57,76 +57,56 @@ public class Synchronizers {
 		}
 
 	}
-	
+
 	/**
 	 * Starts a transition and a synchronizer to modify the specified object's field through the transition.
-	 * @param object The container of the field to be modified.
-	 * @param field The name of the field which contains the object to be modified.
-	 * @param transition The transition to synchronize with the object field.
+	 * 
+	 * @param object
+	 *            The container of the field to be modified.
+	 * @param field
+	 *            The name of the field which contains the object to be modified.
+	 * @param transition
+	 *            The transition to synchronize with the object field.
 	 */
 	public static void transition(Object object, String field, Transition transition) {
-		synchronizedTransitionManager.handle(new TransitionReflectionObjectSynchronizer(transition, object, field));
+		synchronizer.transition(object, field, transition);
 	}
 
 	/**
 	 * Performs the synchronization of all the objects with the corresponding transitions registered by calling transition() method.
 	 */
 	public static void synchronize() {
-		synchronizedTransitionManager.synchronize();
-		transitionHandlersManager.update();
+		synchronizer.synchronize();
 	}
 
 	/**
 	 * Starts a transition and a synchronizer of the transition current value with the specified object. The object must be <b>mutable</b> in order to be modified.
 	 * 
-	 * @param object The <b>mutable</b> object to be modified in through the transition. 
-	 * @param endValue The end value of the transition.
-	 * @param duration The duration of the transition.
+	 * @param object
+	 *            The <b>mutable</b> object to be modified in through the transition.
+	 * @param endValue
+	 *            The end value of the transition.
+	 * @param duration
+	 *            The duration of the transition.
 	 */
 	public static void transition(final Object object, Object endValue, int duration) {
-		final Transition<Object> transition = Transitions.transition(object);
-		transition.set(endValue, duration);
-		transition(object, transition);
+		synchronizer.transition(object, Transitions.transitionBuilder(object).end(endValue).time(duration));
 	}
 
 	/**
 	 * Starts a transition and a synchronizer of the transition current value with the specified object. The object must be <b>mutable</b> in order to be modified.
 	 * 
-	 * @param object The <b>mutable</b> object to be modified in through the transition. 
-	 * @param transition The transition to use to modify the object.
+	 * @param object
+	 *            The <b>mutable</b> object to be modified in through the transition.
+	 * @param transition
+	 *            The transition to use to modify the object.
 	 */
 	public static void transition(final Object object, final Transition transition) {
-
-		synchronizedTransitionManager.handle(new TransitionObjectSynchronizer() {
-
-			@Override
-			public void synchronize() {
-				TypeConverter typeConverter = Converters.converter(object.getClass());
-				Object currentValue = transition.get();
-				float[] x = typeConverter.copyFromObject(currentValue, null);
-				typeConverter.copyToObject(object, x);
-			}
-
-			@Override
-			public boolean isFinished() {
-				return !transition.isTransitioning();
-			}
-
-		});
-
+		synchronizer.transition(object, transition);
 	}
-	
+
 	public static void transition(Object object, Transition transition, TransitionEventHandler transitionEventHandler) {
-		transition(object, transition);
-		transitionHandlersManager.handle(transitionMonitorBuilder.with(transitionEventHandler).monitor(transition).build());
-	}
-	
-	/**
-	 * Returns a monitor builder to use to create a 
-	 * @return
-	 */
-	public static TransitionMonitorBuilder monitorBuilder() {
-		return transitionMonitorBuilder;
+		synchronizer.transition(object, transition, transitionEventHandler);
 	}
 
 }
