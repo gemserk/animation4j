cbcbae50b6597624f44a2e2812347d998ce6087c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-13 10:57:30 -0300

    moved extracted internal class to SynchronizedTransitionManager to add later a free(object) internally after the object was used

--- animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/SynchronizedTransitionManager.java
+++ animation4j-core/src/main/java/com/gemserk/animation4j/transitions/sync/SynchronizedTransitionManager.java
@@ -2,6 +2,8 @@ package com.gemserk.animation4j.transitions.sync;
 
 import java.util.ArrayList;
 
+import com.gemserk.animation4j.transitions.Transition;
+
 /**
  * Handles synchronization of a collection of object synchronizers.
  * 
@@ -10,13 +12,35 @@ import java.util.ArrayList;
  */
 public class SynchronizedTransitionManager {
 
+	private class ObjectSynchronizerProvider {
+		
+		TransitionReflectionObjectSynchronizer getReflectionObjectSynchronizer(Object object, String field, Transition transition) {
+			return new TransitionReflectionObjectSynchronizer(transition, object, field);
+		}
+
+		TransitionDirectObjectSynchronizer getObjectSynchronizer(Object object, Transition transition) {
+			return new TransitionDirectObjectSynchronizer(object, transition);
+		}
+		
+	}
+
 	private ArrayList<TransitionObjectSynchronizer> synchronizers;
 
 	private ArrayList<TransitionObjectSynchronizer> removeSynchronizers;
+	
+	private ObjectSynchronizerProvider objectSynchronizerProvider = new ObjectSynchronizerProvider();
 
 	public SynchronizedTransitionManager() {
 		synchronizers = new ArrayList<TransitionObjectSynchronizer>();
 		removeSynchronizers = new ArrayList<TransitionObjectSynchronizer>();
+	}
+	
+	public void objectSynchronizer(Object object, Transition transition) {
+		handle(objectSynchronizerProvider.getObjectSynchronizer(object, transition));
+	}
+
+	public void reflectionObjectSynchronizer(Object object, String field, Transition transition) {
+		handle(objectSynchronizerProvider.getReflectionObjectSynchronizer(object, field, transition));
 	}
 
 	public void handle(TransitionObjectSynchronizer synchronizer) {
