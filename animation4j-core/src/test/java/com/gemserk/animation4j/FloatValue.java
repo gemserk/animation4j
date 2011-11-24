5ecae68fe504358d4395d6f1ee9efb7550d5d338
Ariel Coppes <ariel.coppes@gemserk.com>
2011-11-24 18:32:32 -0200

    extracted FloatValue and FloatValueConverter used in the tests

---
+++ animation4j-core/src/test/java/com/gemserk/animation4j/FloatValue.java
@@ -0,0 +1,11 @@
+package com.gemserk.animation4j;
+
+public class FloatValue {
+	
+	public float value;
+	
+	public FloatValue(float value) {
+		this.value = value;
+	}
+	
+}
--- animation4j-core/src/test/java/com/gemserk/animation4j/timeline/TimelineValueTest.java
+++ animation4j-core/src/test/java/com/gemserk/animation4j/timeline/TimelineValueTest.java
@@ -7,41 +7,12 @@ import org.hamcrest.core.IsNull;
 import org.hamcrest.core.IsSame;
 import org.junit.Test;
 
-import com.gemserk.animation4j.converters.TypeConverter;
+import com.gemserk.animation4j.FloatValue;
+import com.gemserk.animation4j.FloatValueConverter;
+
 
 
 public class TimelineValueTest {
-	
-	static class FloatValue {
-		
-		public float value;
-		
-		public FloatValue(float value) {
-			this.value = value;
-		}
-		
-	}
-	
-	static class FloatValueConverter implements TypeConverter<FloatValue> {
-
-		@Override
-		public int variables() {
-			return 1;
-		}
-
-		@Override
-		public float[] copyFromObject(FloatValue object, float[] x) {
-			x[0] = object.value;
-			return x;
-		}
-
-		@Override
-		public FloatValue copyToObject(FloatValue object, float[] x) {
-			object.value = x[0];
-			return object;
-		}
-		
-	}
 	
 	FloatValueConverter floatValueConverter = new FloatValueConverter();
 	
