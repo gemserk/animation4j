0139451dac8583328b751d196f1263bd825947f1
Ariel Coppes <ariel.coppes@gemserk.com>
2011-11-24 19:34:48 -0200

    fixed some examples to use new builder and timelines with mutable objects (now the SynchronizedAnimationExampled loses its value)

--- animation4j-examples/src/main/java/com/gemserk/animation4j/examples/SynchronizedAnimationExample.java
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/SynchronizedAnimationExample.java
@@ -1,85 +1,52 @@
 package com.gemserk.animation4j.examples;
 
-import com.gemserk.animation4j.animations.Animation;
 import com.gemserk.animation4j.converters.Converters;
+import com.gemserk.animation4j.timeline.Builders;
 import com.gemserk.animation4j.timeline.TimelineAnimation;
-import com.gemserk.animation4j.timeline.TimelineAnimationBuilder;
-import com.gemserk.animation4j.timeline.TimelineValueBuilder;
-import com.gemserk.animation4j.timeline.sync.ReflectionObjectSynchronizer;
-import com.gemserk.animation4j.timeline.sync.SynchronizedAnimation;
-import com.gemserk.animation4j.timeline.sync.TimelineSynchronizer;
 
 public class SynchronizedAnimationExample {
-	
+
 	public static class MyEntity {
-		
+
 		Vector2f position = new Vector2f(0f, 0f);
-		
 		Vector2f size = new Vector2f(1f, 1f);
-		
-		// you will have to make getters and setters as we are using apache-beanutils to set your fields using reflection.
-		
-		public void setPosition(Vector2f position) {
-			this.position = position;
-		}
-		
-		public Vector2f getPosition() {
-			return position;
-		}
-		
-		public void setSize(Vector2f size) {
-			this.size = size;
-		}
-		
-		public Vector2f getSize() {
-			return size;
-		}
-		
+
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		Converters.register(Vector2f.class, new Vector2fConverter());
-		System.out.println("First example: animation wiht one iteration only");
+		System.out.println("First example: animation with one iteration only");
 		exampleAnimationOneIterationWithDelay();
 	}
 
 	protected static void exampleAnimationOneIterationWithDelay() throws InterruptedException {
 
 		// One problem with time line animations is that you have to get values by hand and setting them to your objects
-		TimelineAnimation timelineAnimation = new TimelineAnimationBuilder() {{
-			
-			value("position", new TimelineValueBuilder(){{
-				keyFrame(0, new Vector2f(100f, 100f));
-				keyFrame(500, new Vector2f(500f, 500f));
-			}});
-			
-			value("size", new TimelineValueBuilder(){{
-				keyFrame(0, new Vector2f(1f, 1f));
-				keyFrame(500, new Vector2f(2f, 2f));
-			}});
 
-			
-		}}.build();
-		
-		// one solution for that (by now) is using an automatic synchronization 
-		
 		MyEntity myEntity = new MyEntity();
-		
-		TimelineSynchronizer timelineSynchronizer = new TimelineSynchronizer(new ReflectionObjectSynchronizer(), myEntity);
-		
-		Animation animation = new SynchronizedAnimation(timelineAnimation, timelineSynchronizer);
+
+		TimelineAnimation animation = Builders.animation(Builders.timeline() //
+				.value(Builders.timelineValue(myEntity.position, new Vector2fConverter()) //
+						.keyFrame(0f, new Vector2f(100f, 100f)) //
+						.keyFrame(0.5f, new Vector2f(500f, 500f)) //
+				) //
+				.value(Builders.timelineValue(myEntity.size, new Vector2fConverter()) //
+						.keyFrame(0f, new Vector2f(1f, 1f)) //
+						.keyFrame(0.5f, new Vector2f(2f, 2f)) //
+				) //
+				) //
+				.build();
+
 		animation.start(1);
-		
-		// using the synchronized animation you shouldn't call the get() method, but for now you still have to call the update method.
-		
+
 		System.out.println("myEntity.position at time 0ms: " + myEntity.position);
 		System.out.println("myEntity.size at time 0ms: " + myEntity.size);
-		
+
 		animation.update(0.25f);
 
 		System.out.println("myEntity.position at time 250ms: " + myEntity.position);
 		System.out.println("myEntity.size at time 250ms: " + myEntity.size);
-		
+
 		animation.update(0.25f);
 
 		System.out.println("myEntity.position at time 500ms: " + myEntity.position);
--- animation4j-examples/src/main/java/com/gemserk/animation4j/examples/TimelineAnimationExample.java
+++ animation4j-examples/src/main/java/com/gemserk/animation4j/examples/TimelineAnimationExample.java
@@ -1,15 +1,14 @@
 package com.gemserk.animation4j.examples;
 
 import com.gemserk.animation4j.converters.Converters;
+import com.gemserk.animation4j.timeline.Builders;
 import com.gemserk.animation4j.timeline.TimelineAnimation;
-import com.gemserk.animation4j.timeline.TimelineAnimationBuilder;
-import com.gemserk.animation4j.timeline.TimelineValueBuilder;
 
 public class TimelineAnimationExample {
 
 	public static void main(String[] args) throws InterruptedException {
 		Converters.register(Vector2f.class, new Vector2fConverter());
-		
+
 		System.out.println("First example: animation with one iteration only");
 		exampleAnimationOneIterationWithDelay();
 		System.out.println("Second example: animation alternating directions");
@@ -18,78 +17,69 @@ public class TimelineAnimationExample {
 
 	protected static void exampleAnimationOneIterationWithDelay() throws InterruptedException {
 
-		TimelineAnimation animation = new TimelineAnimationBuilder() {{
-			
-			delay(100);
-			
-			value("position", new TimelineValueBuilder(){{
-				keyFrame(0, new Vector2f(100f, 100f));
-				keyFrame(500, new Vector2f(500f, 500f));
-			}});
-			
-		}}.build();
-		
+		Vector2f currentValue = new Vector2f(0, 0);
+
+		TimelineAnimation animation = Builders.animation(Builders.timeline() //
+				.value(Builders.timelineValue(currentValue, new Vector2fConverter()) //
+						.keyFrame(0f, new Vector2f(100f, 100f)) //
+						.keyFrame(0.5f, new Vector2f(500f, 500f)) //
+				) //
+				) //
+				.delay(0.1f) //
+				.build();
+
 		// start with only one iteration
 		animation.start(1);
 
-		Vector2f currentValue = animation.getValue("position");
 		System.out.println("currentValue at time 0ms: " + currentValue);
-		
+
 		// value should not be modified because the animation has a delay of 100 units
-		animation.update(99);
-		currentValue = animation.getValue("position");
+		animation.update(0.099f);
 		System.out.println("currentValue at time 99ms: " + currentValue);
-		
-		animation.update(201);
-		currentValue = animation.getValue("position");
+
+		animation.update(0.201f);
 		System.out.println("currentValue at time 300ms: " + currentValue);
 
-		animation.update(300);
-		currentValue = animation.getValue("position");
+		animation.update(0.300f);
 		System.out.println("currentValue at time 600ms: " + currentValue);
-		
-		System.out.println("animation is stopped? : " + animation.isFinished());
-		
-	}
-	
-	protected static void exampleAnimationWithAlternateDirections() throws InterruptedException {
 
-		TimelineAnimation animation = new TimelineAnimationBuilder() {{
-			
-			// note: we haven't defined delay, so delay will be 0 then.
-			
-			value("position", new TimelineValueBuilder(){{
-				keyFrame(0, new Vector2f(100f, 100f));
-				keyFrame(500, new Vector2f(500f, 500f));
-			}});
-			
-		}}.build();
+		System.out.println("animation is stopped? : " + animation.isFinished());
+
+	}
+
+	protected static void exampleAnimationWithAlternateDirections() throws InterruptedException {
 		
+		Vector2f currentValue = new Vector2f(0, 0);
+
+		TimelineAnimation animation = Builders.animation(Builders.timeline() //
+				.value(Builders.timelineValue(currentValue, new Vector2fConverter()) //
+						.keyFrame(0f, new Vector2f(100f, 100f)) //
+						.keyFrame(0.5f, new Vector2f(500f, 500f)) //
+				) //
+				) //
+				.build();
+
 		// start with two iterations and alternating directions.
 		animation.start(2, true);
 
-		Vector2f currentValue = animation.getValue("position");
 		System.out.println("currentValue at time 0: " + currentValue);
 		System.out.println("current direction: " + animation.getPlayingDirection());
-		
+
 		// value should not be modified because the animation has a delay of 100 units
-		animation.update(500);
-		currentValue = animation.getValue("position");
+		animation.update(0.5f);
 		System.out.println("currentValue at time 500: " + currentValue);
 
 		System.out.println("animation is stopped? : " + animation.isFinished());
-		
-		animation.update(100);
-		currentValue = animation.getValue("position");
+
+		animation.update(0.1f);
 		System.out.println("currentValue at time 600: " + currentValue);
 		System.out.println("current direction: " + animation.getPlayingDirection());
 
-		animation.update(1000);
-		currentValue = animation.getValue("position");
+		animation.update(1f);
 		System.out.println("currentValue at time 1000: " + currentValue);
-		
+
 		System.out.println("animation is stopped? : " + animation.isFinished());
-		
+
 	}
 
 }
