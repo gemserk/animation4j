e9a0db062ee382a799d796db94c70ee745c66f6e
Ariel Coppes <ariel.coppes@gemserk.com>
2011-11-24 18:05:20 -0200

    fixed some tests from TimelineTest

--- animation4j-core/src/test/java/com/gemserk/animation4j/timeline/TimelineTest.java
+++ animation4j-core/src/test/java/com/gemserk/animation4j/timeline/TimelineTest.java
@@ -82,63 +82,40 @@ public class TimelineTest {
 		assertEquals(100f, position.y, 0.01f);
 	}
 
-//	@Test
-//	public void testGetValueForAGivenTime() {
-//
-//		Timeline timeline = new Timeline(new HashMap<String, TimelineValue>() {
-//			{
-//				put("myvalue", new TimelineValueBuilder(Converters.floatValue()).keyFrame(0, 100f).keyFrame(100, 200f).build());
-//			}
-//		});
-//
-//		assertEquals(100f, (Float) timeline.getValue(0f, "myvalue"), 0.01f);
-//		assertEquals(200f, (Float) timeline.getValue(0.1f, "myvalue"), 0.01f);
-//	}
-//
-//	@Test
-//	public void testGetValueForAGivenTimeWithDelay() {
-//
-//		Timeline timeline = new Timeline(new HashMap<String, TimelineValue>() {
-//			{
-//				put("myvalue", new TimelineValueBuilder(Converters.floatValue()) //
-//						.keyFrame(0, 100f) //
-//						.keyFrame(100, 200f) //
-//						.build());
-//			}
-//		});
-//
-//		int delay = 100;
-//
-//		assertEquals(100f, (Float) timeline.getValue(0 - delay, "myvalue"), 0.01f);
-//		assertEquals(100f, (Float) timeline.getValue(100 - delay, "myvalue"), 0.01f);
-//		assertEquals(150f, (Float) timeline.getValue(150 - delay, "myvalue"), 0.01f);
-//		assertEquals(200f, (Float) timeline.getValue(200 - delay, "myvalue"), 0.01f);
-//	}
-//
-//	@Test
-//	public void testGarbageGenerationWhenGettingValuesFromTimeline() {
-//
-//		// no new instances should be generated between interpolations when getting the value from the timeline.
-//
-//		Timeline timeline = new Timeline(new HashMap<String, TimelineValue>() {
-//			{
-//				put("myvalue", new TimelineValueBuilder(new Vector2fConverter()).keyFrame(0, new Vector2f(100, 100)).keyFrame(1000, new Vector2f(200, 200)).build());
-//			}
-//		});
-//
-//		int instances = Vector2f.instances;
-//		int arrayInstances = Vector2fConverter.arrayInstances;
-//
-//		System.out.println("vector2f.instances=" + instances);
-//		System.out.println("float[].instances=" + arrayInstances);
-//
-//		for (int i = 0; i < 1000; i++)
-//			timeline.getValue(i, "myvalue");
-//
-//		System.out.println("==== after test ====");
-//		System.out.println("vector2f.instances=" + instances);
-//		System.out.println("float[].instances=" + arrayInstances);
-//
-//	}
+	@Test
+	public void shouldNotGenerateGarbageWhenMovingTheTimeline() {
+
+		// no new instances should be generated between interpolations when getting the value from the timeline.
+
+		Vector2f position = new Vector2f(0f, 0f);
+		
+		final TimelineValue<Vector2f> timelineValue = Builders.timelineValue(position)
+			.typeConverter(new Vector2fConverter()) //
+			.keyFrame(0f, new Vector2f(50f, 0f)) //
+			.keyFrame(1f, new Vector2f(100f, 200f)) //
+			.build();
+		
+
+		Timeline timeline = new Timeline(new ArrayList<TimelineValue>(){{
+			add(timelineValue);
+		}});
+
+		int instances = Vector2f.instances;
+		int arrayInstances = Vector2fConverter.arrayInstances;
+
+		System.out.println("vector2f.instances=" + instances);
+		System.out.println("float[].instances=" + arrayInstances);
+
+		for (int i = 0; i < 5; i++)
+			timeline.move(i);
+
+		System.out.println("==== after test ====");
+		System.out.println("vector2f.instances=" + Vector2f.instances);
+		System.out.println("float[].instances=" + Vector2fConverter.arrayInstances);
+		
+		assertEquals(instances, Vector2f.instances);
+		assertEquals(arrayInstances, Vector2fConverter.arrayInstances);
+
+	}
 
 }
