c3bc5f98006db442cb17029e642e8c36666af6a0
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-01 16:53:13 -0300

    added missing new class

---
+++ animation4j-core/src/main/java/com/gemserk/animation4j/timeline/InternalTimelineValue.java
@@ -0,0 +1,27 @@
+package com.gemserk.animation4j.timeline;
+
+import com.gemserk.animation4j.converters.TypeConverter;
+
+class InternalTimelineValue<T>  {
+
+	private float[] x;
+	private TimelineValueFloatArrayImpl timelineValueFloatArrayImpl;
+
+	private TypeConverter<T> typeConverter;
+
+	public InternalTimelineValue(TypeConverter<T> typeConverter) {
+		this.typeConverter = typeConverter;
+		this.x = new float[typeConverter.variables()];
+		timelineValueFloatArrayImpl = new TimelineValueFloatArrayImpl(x);
+	}
+
+	public void addKeyFrame(KeyFrame keyFrame) {
+		timelineValueFloatArrayImpl.addKeyFrame(keyFrame);
+	}
+
+	public void setTime(T object, float time) {
+		timelineValueFloatArrayImpl.setTime(time);
+		typeConverter.copyToObject(object, x);
+	}
+
+}
